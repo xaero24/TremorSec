b3370f35176feb3f9a009b1b59ad3a29242aca18
Mike A <michaelafu@gmail.com>
2019-04-10 18:35:25 +0300

    updated base files

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import encryptors
+import user
 
 """
 Takes a username and a password and encryprs it.
 The hash is generated and pulled from the server by username - implement.
 Password is compared to locally generated hash - implement.
 
-Unresolved imports - might be VSCode problems?
+Unresolved imports - might be VSCode problems
 """
 #Sign up or login here:
 
 #Sign up - information sent to DB, right now it's a stump
-usrname = input("Enter username: ")
-passwd = input("Enter password: ")
-print("Thanks!")
-code = encryptors.enc(passwd, usrname) #sent to server with username
-
 #Log in - mail pulled later in the process from the DB for 2-stage verification, right now it's a stump
-a_usrname = input("Enter username: ")
-a_passwd = input("Enter password: ")
 
-encryptors.check(a_passwd, a_usrname, code)
+# resp = user.User.check(None, passwd, usrname)
+# if resp == "Wrong password or username":
+#     print(resp)
+#     print("Do you want to sign up? (y/n)")
+#     if input() == 'y':
+#         currentUser = user.User()
+
