83439b9dc4115a06ef1b6a6200a08e4940e0b9c9
Mike <michaelafu@gmail.com>
2019-05-27 16:41:28 +0300

    added connection for 2PC

--- Coordinator.py
+++ Coordinator.py
@@ -1,10 +1,19 @@
-import eladFile as conClass
+import conjnk as conClass
 
 class Coordinator:
-    def __init__():
+    def __init__(self):
         #Use connection strings
-    
-    def tryConnection():
+        self.con = None
+        self.log = {}
 
-
-    def printResponse(respServ):
+    def tryCommit(self, date, value):
+        try:
+            self.con = conClass()
+            self.con.wirteNL(date, value)
+            for (d, v) in self.log:
+                self.con.wirteNL(d, v)
+                self.log.pop(d)
+        except:
+            print("Connection failed, try again later\n")
+            self.log[date] =  value
+        
